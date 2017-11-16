54cccf9219faaba837cb9c9127b44f2898e2b1e5
AndrewRunke <andrewrunke@gmail.com>
2017-11-16 08:01:31 -0800

    Made limit

--- mvcs-homework/src/pages/main.js
+++ mvcs-homework/src/pages/main.js
@@ -9,107 +9,49 @@ var name = (cookies.get('name'));
 var key = (cookies.get('key'));
 console.log(key);
 
-// j is which class it is. It goes from 0-7.
-// i is the class work the maximum is 6 (0-5)
-
-    
-class Andrew {
+class Homework {
   async loadTest() {
     try {
       const response = await fetch('https://classroom.googleapis.com/v1/courses?access_token=' + key);
-      const json = await response.json();     
-      var cwnum = 4;//var to change how many classworks show up
-      for(var j = 0;j<json.courses.length;j++){
-          var rt = 'root'+j; // classname
-          document.getElementById(rt).innerHTML = json.courses[j].name;
-          const coursework = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[j].id+'/courseWork?access_token='+key)
-          const coursejson = await coursework.json();
-          for (var i=0; i < coursejson.courseWork.length; i++ ){
-            if (i<cwnum){
-              var classwk = 'r'+j+'cw'+i; // homework / classwork name
-              document.getElementById(classwk).innerHTML = coursejson.courseWork[i].title;
-            }
-          }
-          
+      const json = await response.json();
+      console.log(json);
+      var ctext ="";
+      var counter1 = 0;
+      var coursework;
+      var coursejson;
+      var courses = [];
+      var counter2 = 0;
+      var coursemax = 4;
+      for(counter1 in json.courses){
+        coursework = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[counter1].id+'/courseWork?access_token='+key);
+        coursejson = await coursework.json();
+        courses[counter1]=coursejson;
       }
-    } catch(err) {
+      console.log(courses);
+      counter1 = 0;
+      for(counter2 in courses){
+        ctext += (Number(counter2)+1)+'. '+'<b>'+json.courses[counter2].name+'</b>'+':'+'<br/>';
+        for(counter1 in courses[counter2].courseWork){
+          if (counter1<coursemax){
+            ctext += "<p>"+courses[counter2].courseWork[counter1].title+"</p>";
+          }
+        }
+      }
+      document.getElementById('root').innerHTML = ctext;
+      } catch(err) {
         console.log(err);
     }
   }
 }
 
-var an = new Andrew();
-an.loadTest();
+var app = new Homework();
+app.loadTest();
 
 const LoginPage = () =>
   <div>
     <h1>Welcome {name}</h1>
 
-    <p id = "root0"></p>
-    <p id = "r0cw0"></p>
-    <p id = "r0cw1"></p>
-    <p id = "r0cw2"></p>
-    <p id = "r0cw3"></p>
-    <p id = "r0cw4"></p>
-    <p id = "r0cw5"></p>
-
-    <p id = "root1"></p>
-    <p id = "r1cw0"></p>
-    <p id = "r1cw1"></p>
-    <p id = "r1cw2"></p>
-    <p id = "r1cw3"></p>
-    <p id = "r1cw4"></p>
-    <p id = "r1cw5"></p>
-
-    <p id = "root2"></p>
-    <p id = "r2cw0"></p>
-    <p id = "r2cw1"></p>
-    <p id = "r2cw2"></p>
-    <p id = "r2cw3"></p>
-    <p id = "r2cw4"></p>
-    <p id = "r2cw5"></p>
-
-    <p id = "root3"></p>
-    <p id = "r3cw0"></p>
-    <p id = "r3cw1"></p>
-    <p id = "r3cw2"></p>
-    <p id = "r3cw3"></p>
-    <p id = "r3cw4"></p>
-    <p id = "r3cw5"></p>
-
-    <p id = "root4"></p>
-    <p id = "r4cw0"></p>
-    <p id = "r4cw1"></p>
-    <p id = "r4cw2"></p>
-    <p id = "r4cw3"></p>
-    <p id = "r4cw4"></p>
-    <p id = "r4cw5"></p>
-
-    <p id = "root5"></p>
-    <p id = "r5cw0"></p>
-    <p id = "r5cw1"></p>
-    <p id = "r5cw2"></p>
-    <p id = "r5cw3"></p>
-    <p id = "r5cw4"></p>
-    <p id = "r5cw5"></p>
-
-    <p id = "root6"></p>
-    <p id = "r6cw0"></p>
-    <p id = "r6cw1"></p>
-    <p id = "r6cw2"></p>
-    <p id = "r6cw3"></p>
-    <p id = "r6cw5"></p>
-
-    <p id = "root7"></p>
-    <p id = "r7cw0"></p>
-    <p id = "r7cw1"></p>
-    <p id = "r7cw2"></p>
-    <p id = "r7cw3"></p>
-    <p id = "r7cw4"></p>
-    <p id = "r7cw5"></p>
-    
-   
-
+    <p id = "root">If this shows then we have a problem or the courses are in console or you need to be patient</p>
   </div>
 
 export default LoginPage
