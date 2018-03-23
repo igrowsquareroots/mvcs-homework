f0ebfc9f090eed04114f4ca15d6c8d109e661f5e
AndrewRunke <andrewrunke@gmail.com>
2018-03-22 20:57:24 -0700

    Shortened code added comments and added feature
    comments to explain what code does
    array with number of assignments per course

--- mvcs-homework/src/pages/main.js
+++ mvcs-homework/src/pages/main.js
@@ -35,42 +35,35 @@ const theme = createMuiTheme ({
 
 });
 
-function isEmpty(obj) {
-  for(var key in obj) {
-      if(obj.hasOwnProperty(key))
-          return false;
-  }
-  return true;
-}
-
 class Homework {
   async loadTest() {
     try {
+      //grab courses
       const response = await fetch('https://classroom.googleapis.com/v1/courses?access_token=' + key);
       const json = await response.json();
-      console.log(json);
-      for(var i = 0; i < json.courses.length; i++) {
-        //console.log("did for "+json.courses[i].name);
-        document.getElementById("class"+(i+1).toString()).innerHTML = json.courses[i].name;
-
-        var res2 = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[i].id+'/courseWork?access_token='+key);
-        var json2 = await res2.json();
-        console.log(json2);
-        if(!isEmpty(json2)) {      
-          var final = "";
-          //console.log(json2.courseWork[0].title);
-          for(var j = 0; j < 5; j++) {
-            final += json2.courseWork[j].title + "</br>";
-            //console.log(final);
+      //coursemax vars will eventually be user defined
+      var coursemax = [1, 1, 1, 1, 1, 1, 1, 1];
+      for(var course =0;course<json.courses.length&&course<9;course++ ){
+        //grab course info
+        var coursework = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[course].id+'/courseWork?access_token='+key);
+        var coursejson = await coursework.json();
+        var assignment ="";
+        for(var assignmentnum in coursejson.courseWork){
+          if (assignmentnum<=coursemax[course]-1){
+            //add in assignment
+            assignment += "<p>"+coursejson.courseWork[assignmentnum].title+"</p>";
           }
-          document.getElementById("class"+(i+1).toString()+"info").innerHTML = final; 
         }
+        //making ids to render
+        document.getElementById('root'+course).innerHTML = assignment;
+        document.getElementById('rootname'+course).innerHTML = json.courses[course].name+'</b>'+':'+'<br/>';;
       }
     } catch(err) {
       console.log(err);
     }
   }
 }
+
 var app = new Homework();
 app.loadTest();
 const LoginPage = () =>
